5b5d1c64aebc4ad508ace51eaa2ad5bef2341184
Aslak Hollund <aslakhol@gmail.com>
2021-09-04 13:38:14 +0200

    link pages from home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import type { NextPage } from "next";
 import Header from "../components/Header";
-import BarList from "../components/bar/BarList";
+import { Link } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <BarList />
+      <Button>
+        <Link href={`/bars`}>Bars</Link>
+      </Button>
+      <Button>
+        <Link href={`/drinks`}>Drinks</Link>
+      </Button>
+      <Button>
+        <Link href={`/ingredients`}>Ingredients</Link>
+      </Button>
+      <Button>
+        <Link href={`/equipment`}>Equipment</Link>
+      </Button>
     </>
   );
 };
