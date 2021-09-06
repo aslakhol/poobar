ca9f86f9dba9952eb69f2aa448179a4bb6448921
Aslak Hollund <aslakhol@gmail.com>
2021-09-06 22:00:13 +0200

    put boxes in a vstack for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,26 @@ import type { NextPage } from "next";
 import Header from "../components/Header";
 import { Link } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
+import { VStack } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <Button>
-        <Link href={`/bar`}>Bars</Link>
-      </Button>
-      <Button>
-        <Link href={`/drink`}>Drinks</Link>
-      </Button>
-      <Button>
-        <Link href={`/ingredient`}>Ingredients</Link>
-      </Button>
-      <Button>
-        <Link href={`/equipment`}>Equipment</Link>
-      </Button>
+      <VStack>
+        <Button>
+          <Link href={`/bar`}>Bars</Link>
+        </Button>
+        <Button>
+          <Link href={`/drink`}>Drinks</Link>
+        </Button>
+        <Button>
+          <Link href={`/ingredient`}>Ingredients</Link>
+        </Button>
+        <Button>
+          <Link href={`/equipment`}>Equipment</Link>
+        </Button>
+      </VStack>
     </>
   );
 };
