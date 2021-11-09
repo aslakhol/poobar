0618de13e257eeabcaa5c2909cac409b48f8defb
Aslak Hollund <aslakhol@gmail.com>
2021-11-09 20:45:40 +0100

    redirect / to testbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { NextPage } from "next";
 import Header from "../components/Header";
-import { Link } from "@chakra-ui/layout";
-import { Button } from "@chakra-ui/button";
-import { VStack } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { Spinner } from "@chakra-ui/spinner";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/bar/1");
+  }, [router]);
+
   return (
     <>
       <Header />
-      <VStack>
-        <Button>
-          <Link href={`/bar`}>Bars</Link>
-        </Button>
-        <Button>
-          <Link href={`/drink`}>Drinks</Link>
-        </Button>
-        <Button>
-          <Link href={`/ingredient`}>Ingredients</Link>
-        </Button>
-        <Button>
-          <Link href={`/equipment`}>Equipment</Link>
-        </Button>
-      </VStack>
+      <Spinner />
     </>
   );
 };
